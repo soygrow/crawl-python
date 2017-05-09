3c7d2f67dc4c1aa9ee6d097409a8c9a16aa952f3
soygrow <308541325@qq.com>
2017-05-09 09:12:46 +0800

    bdtb crawl

--- bdtb_crawl.py
+++ bdtb_crawl.py
@@ -10,15 +10,35 @@ import urllib
 import urllib2
 import re
 
+class Tool:
+    removeImg = re.compile('<img.*?>| {7}|')
+    removeAddr = re.compile('<a.*?>|</a')
+    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
+    replaceTD = re.compile('<td>')
+    replacePara = re.compile('<p.*?>')
+    replaceBR = re.compile('<br><br>|<br>')
+    removeExtraTag = re.compile('<.*?>')
+    br = '<br>'
+    tag = '<.*?>'
+    def replace(self, content):
+        content = re.sub(self.removeImg, "", content)
+        content = re.sub(self.removeAddr, "", content)
+        content = re.sub(self.replaceLine, "\n", content)
+        content = re.sub(self.replaceTD, "\t", content)
+        content = re.sub(self.replaceBR, "\n    ", content)
+        content = re.sub(self.removeExtraTag, "", content)
+        return content
+
 class BDTB:
     def __init__(self, seelZ):
         baseUrl = 'https://tieba.baidu.com/p/3138733512'
         self.baseUrl = baseUrl
         self.seelZ = '?see_lz=' + str(seelZ)
+        self.tool = Tool()
 
     def getPage(self, pageNum):
         try:
-            url = self.baseUrl + self.seelZ
+            url = self.baseUrl + self.seelZ + "&pn=" + str(pageNum)
             request = urllib2.Request(url)
             response = urllib2.urlopen(request)
             page = response.read().decode('utf-8')
@@ -47,18 +67,16 @@ class BDTB:
             return None
 
     def getInfo(self, content):
-        reg = '<div.*?d_author.*?<li.*?d_name.*?<a.*?p_author_name.*?>(.*?)</a>.*?</div>.*?<div.*?post_content.*?>(.*?)</div>'
+        reg = '<div.*?d_author.*?<li.*?d_name.*?<a.*?p_author_name.*?>(.*?)</a>.*?</div>.*?<div id="post_content_.*?>(.*?)</div>.*?<div.*?"post-tail-wrap".*?<span.*?"tail-info">(.*?)</span>.*?<span.*?"tail-info">(.*?)</span>'
         pattern = re.compile(reg, re.S)
         print "==================="
         items = re.findall(pattern, content)
-        print "-------------------"
         infos = []
         for item in items:
-            print item[0]
-            print item[1]
-            #print item[0].strip()
-            #print item[1].strip()
-            #infos.append()
+            print item[0] + " " + item[2] + " " + item[3]
+            #print item[1]
+            print self.tool.replace(item[1])
+            print "\n"
 
     def start(self):
         content = self.getPage(1)
@@ -69,14 +87,14 @@ class BDTB:
         print "Total pages is " + pages
 
         page = 1;
-        while page < pages:
+        print pages
+        while (page <= pages):
+            print pages
+            print "==================BDTB page : " + str(page) + "======================"
             text = self.getPage(page)
             self.getInfo(text)
 
-            page += 1
-            break
-        
-            
+            page += 1            
         
 bdtb = BDTB(1)
 bdtb.start()
